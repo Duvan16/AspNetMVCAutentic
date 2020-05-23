6c7b37f7b26d8ba41d3ff054bc54dcc3ece68af2
Duvan <duvan4448@gmail.com>
2020-05-23 01:07:11 -0500

    14-MiSeguridadMVC

---
+++ AspNetMVCAutentic/AspNetMVCAutentic/Controllers/AccesoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCAutentic.Controllers
+{
+    [Authorize(Users = "dd@gmal.xom")]
+    public class AccesoController : Controller
+    {
+        //[Authorize]
+        public ContentResult Privado()
+        {
+            return Content("Esta parte de la web es PRIVADA");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Publico()
+        {
+            return Content("Esta parte de la web es PUBLICA");
+        }
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
